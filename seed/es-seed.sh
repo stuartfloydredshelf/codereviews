3e896ef17c5f1c18d4b6fc0757fb41277296ca0d
Stuart Floyd <Stuart>
2021-12-04 23:49:45 -0600

    Major changes.

---
+++ seed/es-seed.sh
@@ -0,0 +1,1 @@
+curl -XPOST -u 'redglasses:RedGlasses1234!' 'https://search-rs-campus-tools-2-dev-mfjfrb4mtlasjyfevdertudewm.us-east-1.es.amazonaws.com/_bulk' --data-binary @data.json -H 'Content-Type: application/json'
