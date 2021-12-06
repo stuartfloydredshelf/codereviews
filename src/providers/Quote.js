e32df4eab4215544046647cf9003d9ecac9b8037
Stuart Floyd <Stuart>
2021-12-05 19:46:38 -0600

    Added url

--- src/providers/Quote.js
+++ src/providers/Quote.js
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { updateQuotes } from '../actions/updateQuotes';
+import { addedQuote } from '../actions/addedQuote';
 
 export const emptyQuotes = [
   {
@@ -45,19 +46,7 @@ export const getQuotes = () => async (dispatch) => {
   dispatch(updateQuotes(quotes));
 };
 
-export class QuoteProvider {
-  static async postQuote(quote) {
-    console.log('quote:' + JSON.stringify(quote));
-    return;
-  }
-
-  static async putQuote(quote) {
-    console.log('quote:' + JSON.stringify(quote));
-    return;
-  }
-
-  static async deleteQuote(quote) {
-    console.log('quote:' + JSON.stringify(quote));
-    return;
-  }
-}
+export const addQuote = (author, description) => async (dispatch) => {
+  const quote = {id: 300, author: author, description: description};
+  dispatch(addedQuote(quote));
+};
